37f9204776a8d2a994c3b2a78f3f694f112c6aea
Himanshu Jain <hkj4@dj4.edu>
2020-04-10 15:01:31 +0530

    Updates

---
+++ precache-manifest.1ba6208b14f4585a0dbae5abb0d4eadf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e52c2018ad37868f10181040090239cb",
+    "url": "/react-app-basic/index.html"
+  },
+  {
+    "revision": "955bc69684d01669abff",
+    "url": "/react-app-basic/static/css/main.04441eb6.chunk.css"
+  },
+  {
+    "revision": "7e967c98621b83e785e2",
+    "url": "/react-app-basic/static/js/2.25dcb4ec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-app-basic/static/js/2.25dcb4ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "955bc69684d01669abff",
+    "url": "/react-app-basic/static/js/main.b37b31e1.chunk.js"
+  },
+  {
+    "revision": "4c445f4b8d1b6adc6a1a",
+    "url": "/react-app-basic/static/js/runtime-main.691b99c7.js"
+  }
+]);
